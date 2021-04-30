3cf4547be282ae1bf3faec6fa26583297adde901
linjoehan <linjoehan@yahoo.com>
2021-04-30 06:34:08 +0200

    Had to include a pre built version of the psycopg2 library as the lambda AMI does not include the Postgresql libraries to link to at run time for more info see https://github.com/jkehler/awslambda-psycopg2

--- lambda_setup.bat
+++ lambda_setup.bat
@@ -7,8 +7,9 @@ xcopy .\py_scripts\schools_import.py .\aws_lambda /y
 
 xcopy .\sql_scripts\* .\aws_lambda\sql_scripts\ /y
 
-pip install psycopg2 -t ./aws_lambda
 pip install openpyxl -t ./aws_lambda
+Rem pip install psycopg2 -t ./aws_lambda
+xcopy .\external_libs\psycopg2-3.7\* .\aws_lambda\psycopg2\ /y
 
 del /S aws_lambda.zip /Q
 
